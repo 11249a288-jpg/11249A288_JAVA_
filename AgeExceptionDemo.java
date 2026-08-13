a09298da91a87f872819123b6845dd02d59deee4
11249a288-jpg <11249a288@kanchiuniv.ac.in>
2026-08-13 14:40:06 +0530

    Add files via upload

---
+++ AgeExceptionDemo.java
@@ -0,0 +1,8 @@
+
+CREATE TABLE info (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL,
+    hod_name VARCHAR(100),
+    location VARCHAR(100),
+    total_students INT
+);
