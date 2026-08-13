17e8f80816758e34b0d77574af9ef8a351e9bc13
11249a288-jpg <11249a288@kanchiuniv.ac.in>
2026-08-13 14:39:42 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class add
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the first number");
+a=s.nextInt();
+System.out.println("Enter the second number");
+b=s.nextInt();
+c=a+b;
+System.out.println(c);
+}
+}
