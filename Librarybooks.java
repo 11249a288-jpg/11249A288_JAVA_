3fe10bc5fe9473738bd1255bb40dce555f91fa86
11249a288-jpg <11249a288@kanchiuniv.ac.in>
2026-08-13 15:11:18 +0530

    Add files via upload

---
+++ Librarybooks.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Librarybooks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] books = new String[10];
+
+        System.out.println("Enter 10 book titles:");
+
+        for (int i = 0; i < 10; i++) {
+            books[i] = sc.nextLine();
+        }
+
+        System.out.println("\nBooks starting with letter 'A':");
+
+        for (int i = 0; i < 10; i++) {
+            if (books[i].startsWith("A") || books[i].startsWith("a")) {
+                System.out.println(books[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
