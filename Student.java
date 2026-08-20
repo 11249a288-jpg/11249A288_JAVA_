5170b6fcdb7627f3b3e020d62e691569b3300b9f
11249a288-jpg <11249a288@kanchiuniv.ac.in>
2026-08-20 14:32:59 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,9 @@
+package studentinfo;
+
+public class Student {
+    public void displayInfo() {
+        System.out.println("Student Name: Sanjana");
+        System.out.println("Roll Number: 101");
+        System.out.println("Course: Computer Science");
+    }
+}
